d2a98cd51548d87e0eb2e96dd25c270cbc9804f0
Shilpita <shilpitaroy87@gmail.com>
2017-03-03 21:17:55 -0800

    Trees

--- src/Ex4/BuildOrder.java
+++ src/Ex4/BuildOrder.java
@@ -1,4 +1,11 @@
 package Ex4;
+/**************************************************************************************************************************
+ * Author : Shilpita Roy
+ * Date   : Mar2,2017
+ * Problem: Minimal Tree: Given a sorted (increasing order) array with unique integer elements, 
+ * 			write an algorithm to create a binary search tree with minimal height.
+ *          Cracking the Coding Interview
+ * ***************************************************************************************************************************/
 
 public class BuildOrder {
 
